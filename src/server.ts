33a9dd891399dc6ab73b65e032e70772a3cd4ad6
jun-iper-o <j.lemos@escolar.ifrn.edu.br>
2023-08-24 20:37:03 -0300

    registrando as rotas 'User'

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
 import fastify from "fastify";
 import cors from '@fastify/cors'
 import { appRoutes } from './routes'
+import userRoutes from "./modules/user/user.route";
+import { userSchemas } from "./modules/user/user.schema";
 
 const app = fastify()
 
+
 app.register(cors)
 app.register(appRoutes)
+
+for(const schema of userSchemas){
+    app.addSchema(schema)
+}
+app.register(userRoutes, {prefix:"/users"})
+
+
 
 app.listen({
     port: 3333,
